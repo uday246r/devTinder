26dafb86718f3fc0214c0615a275ac483d607497
uday246r <uday1549.be23@chitkarauniversity.edu.in>
2025-03-27 10:47:32 +0530

    connection_request_basic

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const requestRouter = express.Router();
+
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
 
 requestRouter.post("/sendConnectionRequest",userAuth,  async(req,res) => {
     const user = req.user;
@@ -8,4 +10,35 @@ requestRouter.post("/sendConnectionRequest",userAuth,  async(req,res) => {
     res.send(user.firstName + "sent the connect request!");
 });
 
+requestRouter.post(
+    "/request/send/:status/:toUserId",
+    userAuth,  
+    async(req,res) => {
+        try{
+             const fromUserId = req.user._id;
+             const toUserId = req.params.toUserId;
+             const status = req.params.status;
+
+             const connectionRequest = new ConnectionRequest({
+                fromUserId,
+                toUserId,
+                status,
+             });
+
+             const data = await connectionRequest.save();
+
+             res.json({
+                message: " Connection Request Send Successfully",
+                data,
+            });
+
+        } catch(err){
+            res.status(400).send("ERROR: " + err.message);
+        }        
+    }
+);
+ 
+
 module.exports = requestRouter;
+
+//37.58
