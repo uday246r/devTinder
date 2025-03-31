8328ff5b435b17409cf447bc894f6c402f590b54
uday246r <uday1549.be23@chitkarauniversity.edu.in>
2025-03-31 17:30:05 +0530

    accept_reject_connection

--- apiList.md
+++ apiList.md
@@ -13,9 +13,11 @@
 #connectionRequestRouter
 -POST /request/send/:status/:userId
 <!-- -POST /request/send/ignored/:userId -->
--POST /request/review/accepted/:requestId
--POST /request/review/rejected/:requestId
+-POST /request/review/:status/:requestId
+<!-- -POST /request/review/accepted/:requestId
+-POST /request/review/rejected/:requestId -->
 
+# userRouter
 -GET /connections
 -GET /requests/received
 -GET /feed - Gets you the profiles of other users on platform
--- src/routes/request.js
+++ src/routes/request.js
@@ -63,6 +63,49 @@ requestRouter.post(
         }        
     }
 );
+
+requestRouter.post(
+    "/request/review/:status/:requestId",
+    userAuth,
+    async(req,res) => {
+        try{
+            const loggedInUser = req.user;
+            const { status, requestId } = req.params;
+
+            const allowedStatus = ["accepted", "rejected"];
+            if(!allowedStatus.includes(status)){
+                return res.status(400).json({ message: "Status not allowed!" });
+            }
+
+            const connectionRequest = await ConnectionRequest.findOne({
+                _id: requestId,
+                toUserId: loggedInUser._id,
+                status: "interested",
+            });
+
+            if(!connectionRequest){
+                return res
+                .status(404)
+                .json({ message: "Connection request not found" });
+            }
+
+            connectionRequest.status = status;
+
+            const data = await connectionRequest.save();
+
+            res.json({ message: "Connection request " + status, data });
+            
+            // validate the status
+            // loggedInUser = toUserId
+            // status = interested
+            // request id should be valid
+              
+        } catch(err){
+            res.status(400).send("ERROR: " + err);
+        }
+    }
+
+)
  
 
 module.exports = requestRouter;
